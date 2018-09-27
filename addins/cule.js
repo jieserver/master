5527664d15366484d1732aece0d94f31a1f1fd97
signhuwei <22626612@qq.com>
2018-09-27 11:06:50 +0800

    _ as private status

--- addins/cule.js
+++ addins/cule.js
@@ -11,20 +11,18 @@ function handler(){
     var cules = _.map(cfg.forks,(p,k)=>{
         var i = 0
         p.sourceDir = path.resolve(p.sourceDir)
+        !p.cwd && (p.cwd=p.sourceDir)
         var cule = new Forever('index.js',p)
         cule.on('watch:restart',e=>$log.warn(k,'restart'))
         cule.on('message',m=>$log.debug('get:',k,m,++i))
         cule.start()
-        p.pid = cule.child.pid
+        p['_pid'] = cule.child.pid
         return cule
     })
-    //外部中断
-    process.on('SIGINT',e=>{
+    process.on('SIGINT',e=>{ //外部中断
         $log.debug('SIGINT','kill all')
         cules.forEach(ch=>ch.stop())
-    })
-    //自动退出
-    process.on('exit',e=>{
+    }).on('exit',e=>{ //自动退出
         $log.debug('exit','kill all')
         cules.forEach(ch=>ch.stop())
     })
